608cfdd96152fef6be1566c0a56e9ba8e499df91
kushal <sai.surya225@gmail.com>
2020-08-13 16:08:56 +0530

    custom message to verify registerd user

---
+++ src/api/email-templates/verificationEmail.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="row">
+<div class="col-lg-12">
+  <h5 class="mb-4">
+    <strong>Verify mail</strong>
+  </h5>
+  <div class="mb-5">
+    <div
+      style={{
+        background: '#eceff4',
+        padding: '50px 20px',
+        color: '#514d6a',
+        borderRadius: '5px',
+      }}
+    >
+      <div style={{ maxWidth: '700px', margin: '0px auto', fontSize: '14px' }}>
+        <table
+          cellPadding="0"
+          cellSpacing="0"
+          style={{ width: '100%', marginBottom: '20px', border: '0px' }}
+        >
+        </table>
+        <div style={{ padding: '40px 40px 20px 40px', background: '#fff' }}>
+          <table cellPadding="0" cellSpacing="0" style={{ width: '100%', border: '0px' }}>
+            <tbody>
+              <tr>
+                <td>
+                  <p>Hi there,</p>
+                  <p>
+                    Please Click on the Button to Verify Your Account
+                  </p>
+                  <a class="btn btn-primary" href=${Link}>Click Here</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+     </div>
+    </div>
+  </div>
+ </div>
+</div>
+</body>
+</html>`;
+
--- src/api/utils/mailer.js
+++ src/api/utils/mailer.js
@@ -2,7 +2,6 @@ const express=require('express')
 const nodemailer = require("nodemailer")
 const app = express()
 require('dotenv').config()
-
 const smtpTransport = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     auth: {
@@ -33,7 +32,63 @@ module.exports.sendVerificationEmail = async (host,email,username,token) => {
 	let mailOptions={
         to : email,
         subject : `Verify Email`,
-        html : `Dear ${username},<br> Please click <a href="${link}">here</a> to verify your email.`
+		html : `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		  <meta charset="UTF-8"/>
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		</head>
+		<body>
+		<div className="row">
+		<div className="col-lg-12">
+		  <h5 className="mb-4">
+			<strong>Verify Email</strong>
+		  </h5>
+		  <div className="mb-5">
+			<div
+			  style={{
+				background: '#eceff4',
+				padding: '50px 20px',
+				color: '#514d6a',
+				borderRadius: '5px',
+			  }}
+			>
+			  <div style={{ maxWidth: '700px', margin: '0px auto', fontSize: '14px' }}>
+				<table
+				  cellPadding="0"
+				  cellSpacing="0"
+				  style={{ width: '100%', marginBottom: '20px', border: '0px' }}
+				>
+				</table>
+				<div style={{ padding: '40px 40px 20px 40px', background: '#fff' }}>
+				  <table cellPadding="0" cellSpacing="0" style={{ width: '100%', border: '0px' }}>
+					<tbody>
+					  <tr>
+						<td>
+						  <p>Hi there,</p>
+						  <p>
+							Please Click on the button  belowto Verify Your Account
+						  </p>
+						  <a href=${link}  className="btn btn-primary">Click here to verify</a>
+              
+					  
+						</td>
+					  </tr>
+					</tbody>
+				  </table>
+				</div>
+			 </div>
+			</div>
+		  </div>
+		 </div>
+		</div>
+		</body>
+		</html>`
+		//`Dear ${username},<br> Please click <a href="${link}">here</a> to verify your email.`
     }
     smtpTransport.sendMail(mailOptions, function(error, response){
 		if(error){
