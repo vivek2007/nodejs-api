a94163f556dc7a1efaa48844d20cb380baa56ab0
kushal <sai.surya225@gmail.com>
2020-08-14 14:11:21 +0530

    custom templates for veriication, reset password, forgot password

--- src/api/utils/mailer.js
+++ src/api/utils/mailer.js
@@ -1,7 +1,20 @@
 const express=require('express')
 const nodemailer = require("nodemailer")
+const path = require('path')
+const Hogan = require('hogan.js')
+const fs = require('fs')
 const app = express()
 require('dotenv').config()
+
+const verifyEmail =  fs.readFileSync('./src/api/views/verifyEmail.hjs',{encoding:'utf8', flag:'r'}); 
+const VerifyEmail = Hogan.compile(verifyEmail)
+
+const referalCode =  fs.readFileSync('./src/api/views/referalcode.hjs',{encoding:'utf8', flag:'r'}); 
+const referalcodeEmail = Hogan.compile(referalCode)
+
+const resetpassword =  fs.readFileSync('./src/api/views/resetpassword.hjs',{encoding:'utf8', flag:'r'}); 
+const resetpasswordEmail = Hogan.compile(resetpassword)
+
 const smtpTransport = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     auth: {
@@ -15,7 +28,7 @@ module.exports.sendReferralCode = async (host,email,username,referralCode) => {
 	let mailOptions={
         to : email,
         subject : `Email Verified Success`,
-        html : `Dear ${username},<br> Thank you for registering with Clicks. Your email has been verified successfully. Please save this referral code <b>${referralCode}</b>. This referral code can be used at the time of signup of your friends and you will get benefits from clicks.`
+		html : referalcodeEmail.render({name = `${username}`, code = `${referralCode}` })
     }
     smtpTransport.sendMail(mailOptions, function(error, response){
 		if(error){
@@ -32,60 +45,7 @@ module.exports.sendVerificationEmail = async (host,email,username,token) => {
 	let mailOptions={
         to : email,
         subject : `Verify Email`,
-		html :`<html>
-		<head>
-		  <meta charset="UTF-8"/>
-		  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
-		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-		</head>
-		<body>
-		<div className="row">
-		<div className="col-lg-12">
-		  <h5 className="mb-4">
-			<strong>Verify Email</strong>
-		  </h5>
-		  <div className="mb-5">
-			<div
-			  style={{
-				background: '#eceff4',
-				padding: '50px 20px',
-				color: '#514d6a',
-				borderRadius: '5px',
-			  }}
-			>
-			  <div style={{ maxWidth: '700px', margin: '0px auto', fontSize: '14px' }}>
-				<table
-				  cellPadding="0"
-				  cellSpacing="0"
-				  style={{ width: '100%', marginBottom: '20px', border: '0px' }}
-				>
-				</table>
-				<div style={{ padding: '40px 40px 20px 40px', background: '#fff' }}>
-				  <table cellPadding="0" cellSpacing="0" style={{ width: '100%', border: '0px' }}>
-					<tbody>
-					  <tr>
-						<td>
-						  <p>Hi there,</p>
-						  <p>
-							Please Click on the Link  below to Verify Your Account
-						  </p>
-						  <a href=${link} className="btn btn-primary">Click here to verify</a>
-						  </td>
-					  </tr>
-					</tbody>
-				  </table>
-				</div>
-			 </div>
-			</div>
-		  </div>
-		 </div>
-		</div>
-		</body>
-		</html>`
-		
+		html : 	VerifyEmail.render({link :`${link}`})
 		// `Dear ${username},<br> Please click <a href="${link}">here</a> to verify your email.`
     }
     smtpTransport.sendMail(mailOptions, function(error, response){
@@ -103,17 +63,7 @@ module.exports.sendResetPassword = async (host,email,username,token) => {
 	let mailOptions={
         to : email,
         subject : `Reset Password`,
-        html : `<tbody>
-		<tr>
-		  <td>
-			<p>Hi there,</p>
-			<p>
-			  Please Click on the Link  below to Reset password of your Account
-			</p>
-			<a href=${link} className="btn btn-primary">Click here to reset password</a>
-			</td>
-		</tr>
-	  </tbody>`
+        html : resetpasswordEmail.render({link :`${link}`})
     }
     smtpTransport.sendMail(mailOptions, function(error, response){
 		if(error){
